9e82e92469b2ecf24dd0407081e52ad4b30c03f7
Alexei Sholik <alcosholik@gmail.com>
2014-09-22 01:37:09 +0300

    Remove outdated files

--- configure.sh
+++ configure.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-if [ "$#" -ne 3 ]; then
-    echo "usage: ./configure.sh <path to elixir dir> <path to elixir site dir> \
-        <path to ex_doc dir>"
+if [ "$#" -ne 1 ]; then
+    echo "usage: ./configure.sh <path to elixir site dir>"
     exit 1
 fi
 
 cat Makefile.in \
-    | sed "s,ELIXIR_DIR =,ELIXIR_DIR = $1," \
-    | sed "s,ELIXIR_LANG_DIR =,ELIXIR_LANG_DIR = $2," \
-    | sed "s,EXDOC_DIR =,EXDOC_DIR = $3," \
+    | sed "s,ELIXIR_LANG_DIR =,ELIXIR_LANG_DIR = $1," \
     > Makefile
